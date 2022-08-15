f4ce87648322e03ef8ca9ddc713ada0889c0ca22
Julian Mills <julian.exodius@gmail.com>
2022-08-15 20:58:53 +0200

    fix: example site issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path';
-import react from '@vitejs/plugin-react'
-import typescript from '@rollup/plugin-typescript'
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
 	plugins: [
 		react(),
-		typescript({
-			'declaration': true,
-			'declarationDir': resolve(__dirname, 'dist'),
-		})
+		dts()
 	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'lib/index.ts'),
 			name: 'react-router-tree',
 			fileName: 'react-router-tree'
-		},
-		rollupOptions: {
-			external: [
-				'react',
-				'react-router',
-				'react-router-dom'
-			]
 		}
 	}
 })
